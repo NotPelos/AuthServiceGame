e97228c0206aaaf31e1561f190b29b0d4f7eb534
NotPelos <iezzio4@gmail.com>
2024-10-31 12:26:03 +0100

    Añadir imports

--- src/main/java/com/example/authservice/infrastructure/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/authservice/infrastructure/config/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.HashMap;
 import java.util.Map;
 
